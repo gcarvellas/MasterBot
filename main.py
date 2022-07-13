d2bc8adb80641bad0ff4d55ee3010e18f3c78031
gcarvellas <gcarvellas@castlepointanime.com>
2022-07-13 17:53:46 -0400

    async downloader (WIP)

--- main.py
+++ main.py
@@ -6,8 +6,3 @@ from sites.danbooru import Danbooru
 
 def main():
     pass  # TODO call sites and init bot
-
-start_time = time.time()
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-asyncio.new_event_loop().run_until_complete(Danbooru().download_random_images(500))
-print(f"Multi Threaded: {time.time() - start_time}")
